de227e8bc9110d44958856b5a5802c07a13b2542
Rawgher <rdpender3@gmail.com>
2024-06-07 08:44:28 -0700

    model and seed file working

--- models.py
+++ models.py
@@ -1,1 +1,20 @@
 """Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    """Connect to the db."""
+    db.app = app
+    db.init_app(app)
+
+class Cupcake(db.Model):
+    """Cupcake Model"""
+
+    __tablename__ = 'cupcakes'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    flavor = db.Column(db.Text, nullable=False)
+    size = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    image = db.Column(db.Text, nullable=True, default='https://thestayathomechef.com/wp-content/uploads/2017/12/Most-Amazing-Chocolate-Cupcakes-1-small.jpg')
